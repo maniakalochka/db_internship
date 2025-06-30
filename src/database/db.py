86f071fe938aa3fec68474002a6ea342565e7079
Maniakalochka <prochorwolochoff@gmail.com>
2025-06-30 18:32:07 +0300

    Remove unused model imports from db.py

--- src/database/db.py
+++ src/database/db.py
@@ -2,16 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from core.config import settings
-from models.author import Author
-from models.base import Base
-from models.book import Book
-from models.buy import Buy
-from models.buy_book import BuyBook
-from models.buy_step import BuyStep
-from models.city import City
-from models.client import Client
-from models.genre import Genre
-from models.step import Step
+
 
 engine = create_engine(
     url=settings.DB_URL,
